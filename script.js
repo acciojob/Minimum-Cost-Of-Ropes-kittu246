620b747c7cf9f511dc27220828c7f5682abcd5aa
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-13 13:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function calculateMinCost() {
+function calculateMinCost(n, arr) {
   //your code here
+  let finalsum =0;
   
+    while(n>1){
+      arr.sort((a,b) => a-b);
+
+      let sum = arr[0]+arr[1];
+
+      //console.log(sum);
+
+      arr.splice(0,2,sum);
+
+      finalsum = finalsum + sum  ;
+      n--;
+
+    }
+
+    return finalsum;
   
   
 }  
